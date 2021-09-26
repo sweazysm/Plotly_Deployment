b0c0e6c2ccb78a66ebf82055a306954a4451652d
Scott Sweazy Jr <86274124+sweazysm@users.noreply.github.com>
2021-09-26 11:43:48 -0400

    Add files via upload

---
+++ spaceX.js
@@ -0,0 +1,10 @@
+const url = "https://api.spacexdata.com/v2/launchpads";
+
+// d3.json(url).then(receivedData => console.log(receivedData));
+
+// d3.json(url).then(spaceXResults =>
+//     console.log(spaceXResults[0].location.latitude));
+
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
